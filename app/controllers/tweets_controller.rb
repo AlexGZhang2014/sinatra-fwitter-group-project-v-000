8dd6eead35f05cf5a597756096b328371c94d16a
Alex Zhang <alexgzhang92@gmail.com>
2018-06-28 22:20:08 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,8 @@ class TweetsController < ApplicationController
   get '/tweets/new' do
     erb :'tweets/new'
   end
+  
+  post '/tweets' do
+    @tweet = Tweet.create(content: params[:content])
+  end
 end
