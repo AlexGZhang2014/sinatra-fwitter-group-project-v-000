f7b55e9629c7203a3844b54dde5a2c21b83bb83b
Alex Zhang <alexgzhang92@gmail.com>
2018-06-28 23:23:41 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,14 +43,4 @@ class UsersController < ApplicationController
     session.clear
     redirect '/'
   end
-  
-  helpers do
-		def logged_in?
-			!!session[:user_id]
-		end
-
-		def current_user
-			User.find(session[:user_id])
-		end
-	end
 end
