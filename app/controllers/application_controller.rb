aa5822d4f468cc22405de466b5093a93e32f6af1
Alex Zhang <alexgzhang92@gmail.com>
2018-06-28 23:41:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,6 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-  end
-  
-  configure do
     enable :sessions
     set :session_secret, "password_security"
   end
