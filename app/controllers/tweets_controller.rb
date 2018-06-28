70c44a1c31b5506a70c2a16fb9cab5c0fa851bd1
Alex Zhang <alexgzhang92@gmail.com>
2018-06-28 22:37:40 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,8 +11,10 @@ class TweetsController < ApplicationController
   post '/tweets' do
     if !params[:content].empty?
       @tweet = Tweet.create(content: params[:content])
+      redirect to "/tweets/#{@tweet.id}"
+    else
+      redirect to "/tweets/new"
     end
-    redirect to "/tweets/#{@tweet.id}"
   end
   
   get '/tweets/:id' do
